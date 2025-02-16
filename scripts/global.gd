839c3d56bb173fd05d54945f472f6d14945ff7c0
Coffee and Errors <170835528+FinalFenetix@users.noreply.github.com>
2025-02-17 07:12:53 +1000

    feat: Added main menu, options button not working, Added coin button and text displaying coin count, added global script

---
+++ scripts/global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var coins:= 0
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
---
+++ scripts/texture_button.gd
@@ -0,0 +1,6 @@
+extends TextureButton
+
+
+
+func _on_pressed() -> void: # On self pressed
+	Global.coins += 1
